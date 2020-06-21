596c33e57ccf4baefe82a413ee87e73499275cd3
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-21 22:27:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,6 +25,7 @@ class SongsController < ApplicationController
   end
 
   def new
+    @song = Song.new
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
       redirect_to artists_path, alert: "Artist not found."
     else
